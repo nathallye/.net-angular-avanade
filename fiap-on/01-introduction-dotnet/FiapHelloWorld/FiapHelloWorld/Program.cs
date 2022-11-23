ef6e4a6c7b6f30204946615d67afc0c1ee594ea8
nathallyet <nathallyet@gmail.com>
2022-11-23 14:26:53 -0300

    fiap on - project FiapHelloWorld

---
+++ fiap-on/01-introduction-dotnet/FiapHelloWorld/FiapHelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FiapHelloWorld.Models;
+
+namespace FiapHelloWorld // namespace
+{
+    class Program // classe
+    {
+        static void Main(string[] args) // método
+        {
+            HelloModel helloModel = new Models.HelloModel();
+            Console.WriteLine(helloModel.Message);
+
+            Console.Read(); // trecho para manter a janela aberta
+        }
+
+    }
+}
