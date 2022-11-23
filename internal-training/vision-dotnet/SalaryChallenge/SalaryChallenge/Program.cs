9f6d140554add0d0d86714452707e552157b439e
nathallyet <nathallyet@gmail.com>
2022-11-23 16:18:33 -0300

    internal training - salary challemge

---
+++ internal-training/vision-dotnet/SalaryChallenge/SalaryChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalaryChallenge
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            double salary = 0;
+            double adjustment = 0;
+            double salaryWithAdjustment = 0;
+
+            Console.WriteLine("Entre com o salário atual do colaborador: ");
+            salary = double.Parse(Console.ReadLine());
+
+            adjustment = salary * 0.10;
+            salaryWithAdjustment = salary + adjustment;
+
+            Console.WriteLine("O salário ajustado do colaborador é: " + salaryWithAdjustment.ToString("F2"));
+        }
+    }
+}
